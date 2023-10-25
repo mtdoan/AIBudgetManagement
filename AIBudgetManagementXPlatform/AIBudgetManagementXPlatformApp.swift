560f640c5c60bc4f0da91915dc58e60a67cb4414
Mindy Doan <mtdoan0109@gmail.com>
2023-10-25 20:57:00 +1100

    Initial Commit

---
+++ AIBudgetManagementXPlatform/AIBudgetManagementXPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIBudgetManagementXPlatformApp.swift
+//  AIBudgetManagementXPlatform
+//
+//  Created by Mindy Doan on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AIBudgetManagementXPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIBudgetManagementXPlatform/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIBudgetManagementXPlatform
+//
+//  Created by Mindy Doan on 25/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
